dc87a2b5fb08c8e4d3e9901d189505e29604e9e4
gauravpatil78 <140796180+gauravpatil78@users.noreply.github.com>
2023-09-29 06:13:29 +0000

    objects

--- basic2_js/objects.js
+++ basic2_js/objects.js
@@ -7,12 +7,39 @@ const employee={
        degree:["diploma","engineering"]
 }
 
-console.log(employee.degree);
-console.log(employee["degree"]);
+// console.log(employee.degree);
+// console.log(employee["degree"]);
 
 
 employee.gre=function(){
     console.log(`hello ${this.name}`);
 }
-console.log(employee.gre());
-console.log(employee);
+// console.log(employee.gre());
+// console.log(employee);
+
+
+
+
+//objct 2 lec
+
+const user=new Object();
+
+user.id=102;
+user.name="gaurav";
+// console.log(user)
+
+user.Info={
+    firstname:{
+        name:"nikhli",
+        lastname:{
+            last:"thorat",
+            city:{
+                name:"Kalyan"
+            }
+        }
+    }
+}
+
+console.log(user.Info.firstname.lastname.city.name)
+
+
