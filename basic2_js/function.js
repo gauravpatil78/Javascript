f7612b1ae19d3d19f720580d8dd386fe75cb10b3
gauravpatil78 <140796180+gauravpatil78@users.noreply.github.com>
2023-10-12 11:21:21 +0000

    function

--- basic2_js/function.js
+++ basic2_js/function.js
@@ -7,3 +7,32 @@ function addNum(num1,num2){
 const res=addNum(20,30);
 
 console.log(res);
+
+const myObj={
+    username :"gaurav",
+    id:1,
+    password:"12345678"
+}
+
+function callObj(obj){
+
+    return `${obj.username} is my name and ${obj.id} is my id ${obj.password} is may password`;
+}
+
+console.log(callObj(myObj));
+
+
+
+function details(obj){
+console.log(obj.id);
+    if(obj.id===1){
+        return obj.name;
+    }else{
+        return "user not available";
+    }
+}
+
+console.log(details({
+    id:1,
+    name:"Nikhil"
+}))
